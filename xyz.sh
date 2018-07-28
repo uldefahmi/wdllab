1e9a167526c675acc3114e288f28b9d957530b19
uldefahmi <fahmiulde@gmail.com>
2018-07-29 00:54:34 +0530

    xyz.sh

---
+++ xyz.sh
@@ -0,0 +1,15 @@
+git filter-branch --env-filter '
+WRONG_EMAIL="kotharihozefa53@gmail.com"
+NEW_NAME="Safiya Naaz"
+NEW_EMAIL="safiya.f30@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$NEW_NAME"
+    export GIT_COMMITTER_EMAIL="$NEW_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$WRONG_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$NEW_NAME"
+    export GIT_AUTHOR_EMAIL="$NEW_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
